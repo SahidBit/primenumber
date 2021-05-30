601ec3e10d74b72ae8a967e6c1c018ed3847f3d4
SahidBit <52377434+SahidBit@users.noreply.github.com>
2021-05-30 23:41:16 +0300

    Accept User Input

--- PrimeNumber/src/primenumber.java
+++ PrimeNumber/src/primenumber.java
@@ -1,4 +1,18 @@
 
+import java.util.Scanner;
+
 public class primenumber {
+	public static void main(String[]args) {
+		
+		
+		
+		Scanner input= new Scanner (System.in);
+		
+		System.out.print("Enter any Number");
+		input.nextInt();
+		input.close();
+		
+	}
+	
 
 }
