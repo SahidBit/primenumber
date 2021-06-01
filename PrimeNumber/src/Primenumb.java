d056731b1eada865a6b698835623d3a0fdd3e0e8
SahidBit <52377434+SahidBit@users.noreply.github.com>
2021-06-01 10:08:37 +0300

    prime or composite with iteration number

--- PrimeNumber/src/Primenumb.java
+++ PrimeNumber/src/Primenumb.java
@@ -4,33 +4,20 @@ public class Primenumb {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
+
 		int number;
 		boolean prime = true;
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please Enter the number");
+		System.out.println(" please gave a number:");
 		number = input.nextInt();
 		input.close();
-		
-		// iteration
-		
-		for (int i=2; i<number; i++) {
-			System.out.println ("Iteration: " + i );
-			if (number % i ==0) {
-				prime = false;
-				break;
-			}
-			
-		}
 		
 		if (number < 1)
 			System.out.println("Number need to be greater than 1");
 		else if (number == 1)
 			System.out.println("1 is neither Prime nor Composite");
 		else {
-			for (int divisor = 2; divisor <= (number/2); divisor ++ ) {
+			for (int divisor = 2; divisor <= (number/2); ) {
 				prime = false;
 				break;
 				
@@ -40,6 +27,18 @@ public class Primenumb {
 			System.out.printf("%d is a prime", number);
 		else 
 			System.out.printf("%d is a Composite",number);
+		// iteration
+		
+		for (int i=2; i<number; i++) {
+			System.out.println (" with 1st Iteration: " + i );
+			if (number % i ==0) {
+				prime = false;
+				break;
+			}
+			
+		}
+		
+		
 		
 	}
 
